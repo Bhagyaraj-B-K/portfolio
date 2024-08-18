8553cf65d63be477047bf447da85456221677de8
Bhagyaraj-B-K <bhagyarajbk7@gmail.com>
2024-08-18 15:36:01 +0530

    ADDED: Education Page added

---
+++ src/constants/education.ts
@@ -0,0 +1,9 @@
+export default {
+  degree: 'Bachelor of Computer Science',
+  major: 'Computer Science',
+  university: 'Vivekananda College of Engineering and Technology, Puttur',
+  graduationYear: 2022,
+  gpa: 8.5,
+  summary:
+    'I hold a B.Tech degree from Vivekananda College of Engineering and Technology, Puttur, where I honed my technical skills and laid the foundation for my career in software development. Prior to that, I completed my pre-university education at Vivekananda Pre University College, Puttur, where I developed a strong base in science and mathematics. My schooling years were spent at SVS Temple English Medium School, Bantwal, where my academic journey began, and I cultivated a passion for learning.'
+}
--- src/modules/Education.tsx
+++ src/modules/Education.tsx
@@ -1,5 +1,23 @@
+import './Education.css'
+import education from '../constants/education'
+
 function Education() {
-  return <></>
+  return (
+    <>
+      <div className='education'>
+        <h2 style={{ textAlign: 'center', marginTop: '5%', textDecoration: 'underline wavy 1.5px' }}>Education</h2> <hr />
+        <img className='graduation-image' src='/graduation.png' alt='Education' width='60%' /> <hr />
+        <div className='education-details'>
+          <h4>
+            {education.degree}, {education.graduationYear}
+          </h4>
+          <p>{education.university}</p>
+          <p>GPA: {education.gpa}</p>
+          <p style={{ fontSize: '8px' }}>{education.summary}</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Education
