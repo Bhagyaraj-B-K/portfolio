b9813c43c4b13b8d6b9d8c1e1c04d32f90c9d865
Bhagyaraj-B-K <bhagyarajbk7@gmail.com>
2024-09-20 14:03:10 +0530

    ADDED: Footer to the ending page

--- src/modules/Ending.tsx
+++ src/modules/Ending.tsx
@@ -1,4 +1,5 @@
 import './Ending.css'
+import { bio } from '../constants'
 
 function Ending() {
   return (
@@ -7,6 +8,9 @@ function Ending() {
         <div className='center-piece'>
           <p style={{ fontSize: '3.5rem' }}>Thanks</p>
           <p style={{ marginTop: '-7%', marginLeft: '20%' }}>for Stopping by...</p>
+        </div>
+        <div className='magazine-ending-footer'>
+          <p>{bio.user.name}</p>
         </div>
       </div>
     </div>
