250cb4be31cbb5dfc24f830f61fbe7eb43c0359e
Bhagyaraj-B-K <bhagyarajbk7@gmail.com>
2024-09-21 20:32:37 +0530

    UPDATE: Korus project description

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -5,7 +5,7 @@ export default {
     link: 'https://play.korus.co/',
     description: `Worked as the solo Backend developer and engineer for Korus Universe, a project outsourced to Pixelynx. 
     The project focused on generating music tracks and videos by combining musical stems, with AI integration provided by Soundmosaic service. 
-    I enabled the minting of music assets through web3 technologies using Mintfactory APIs and streamlined the user experience by integrating invisible wallet creation via ThirdWeb. Utilized technologies such as NestJs, DynamoDB, MySQL, Redis, Jest for testing, and Bitbucket for repository management and CICD pipelines. The servers were deployed using Render servers`,
+    I enabled the minting of music assets through web3 technologies using Mintfactory APIs and streamlined the user experience by integrating invisible wallet creation via ThirdWeb. Utilized technologies such as NestJs, DynamoDB, MySQL, Redis, Jest for testing, and Bitbucket for repository management and CICD pipelines. The servers were deployed using Render.`,
     images: ['korus.jpg', 'koruslogo.jpg', 'koruslogo.png']
   },
   SCS: {
